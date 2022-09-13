f22fa1b49b8fcb1f0c6caf2933c9a6347bf9e121
marilia-borgo <marilia.bm01@gmail.com>
2022-09-13 14:10:29 -0300

    adicionei a página para adicionar nova tarefa'

--- src/views/TasksListView.vue
+++ src/views/TasksListView.vue
@@ -1,37 +1,65 @@
 <template>
+  <div>
+    <v-btn
+      class="mx-2 ml-15 mt-8"
+      fab
+      dark
+      color="indigo"
+     
+    >
+      <v-icon dark>
+        mdi-plus
+      </v-icon>
+    </v-btn>
   <v-row justify="center" class="pa-md-16 mx-lg-auto">
 
     <v-expansion-panels inset>
       <v-expansion-panel v-for="tarefa in tasks" :key="tarefa.id">
-        <v-expansion-panel-header> {{ tarefa.title }} </v-expansion-panel-header>
+        <v-expansion-panel-header class='ml-5'> {{ tarefa.title }} </v-expansion-panel-header>
         <v-expansion-panel-content>
-          <v-container></v-container>
+          <v-container>
+            <v-row no-gutters justify="space-between ">
+      <v-col
+        cols="12"
+        sm="12"
+        md="11"
+        class='ml-n16'
+      >
           <v-chip
       class="ma-2"
       color=deep-purple lighten-4
       label
       text-color="white"
     >
+ 
       <v-icon left>
         mdi-label
       </v-icon>
       {{tarefa.project}}
     </v-chip>
+  </v-col>
+  <v-col
+        cols="auto"
+        md="1"
+        class='mr-1'
+        
+      >
 
     <v-checkbox
               v-model="ex4"
-             
               color=deep-purple lighten-4
               value="indigo"
               hide-details
-              left
+              
             ></v-checkbox>
-
+          </v-col>
+    </v-row>
          </v-container>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
   </v-row>
+</div>
 </template>
 
 <script>
---
+++ src/views/addTaskView.vue
@@ -0,0 +1,89 @@
+<template>
+    <v-card
+    class="mx-auto px-16 py-16 mx-5 my-15"
+    max-width="344"
+    outlined>
+  <v-text-field
+            v-model="message4"
+            label="Add Task"
+            outlined
+            clearable
+          ></v-text-field>
+
+          <v-combobox
+          v-model="select"
+          :items="items"
+          label="Tipo de projeto"
+          multiple
+          chips
+        > </v-combobox>
+        <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="date"
+            label="Data de entrega"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="date"
+          no-title
+          scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.menu.save(date)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+      <v-btn
+      class="ma-2"
+      outlined
+      color="indigo"
+    >
+      Adicionar tarefa
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        select: [],
+        items: [
+          'Front-end',
+          'Back-end',
+          'Dev-ops',
+        ],
+      }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
