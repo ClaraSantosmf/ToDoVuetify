ca4dc88c82286fe80ceedefd7eb916a6583f3492
ClaraSantosmf <clarasantosmf@gmail.com>
2022-09-13 09:45:41 -0300

    Lógicas do AXIOS

---
+++ TasksAPi.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default {
+  getTasks: (callback) => {
+    axios.get("http://localhost:3000/tasks/").then((response) => {
+      callback(response.data);
+    });
+  },
+  petTasks: (task) => {
+    axios.put(`http://localhost:3000/tasks/${task.id}`, task);
+  },
+  postTasks: (task) => {
+    axios.post("http://localhost:3000/tasks/", task);
+  },
+  deleteTasks: (task) => {
+    axios.delete(`http://localhost:3000/tasks/${task.id}`);
+  },
+};
