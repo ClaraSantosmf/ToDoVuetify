c34dc30f18d0f9c97750d1018cc49dbfaa1401bb
marilia-borgo <marilia.bm01@gmail.com>
2022-09-13 09:46:30 -0300

    get não funciona

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,9 @@
       <v-row justify="center" class="pa-md-16 mx-lg-auto">
     <v-expansion-panels inset>
       <v-expansion-panel
-        v-for="(item,i) in 5"
-        :key="i"
-      >
-        <v-expansion-panel-header>Item</v-expansion-panel-header>
+      v-for="tarefa in tasks" 
+      :key="tarefa.id">
+        <v-expansion-panel-header> {{ tarefa }} </v-expansion-panel-header>
         <v-expansion-panel-content>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </v-expansion-panel-content>
@@ -16,15 +15,39 @@
 
 
 <script>
-
-
+import axios from 'axios'
 export default {
+ 
   name: "HomeView",
+  data: function(){
+  return{
+    tasks: [],
+    task: {
+      "id": '',
+      "title":'',
+      "dueTo": '',
+      "project": '',
+      "usuario": '',
+      isShow:false,
+    },
+  }
+},
+methods:{
+getTasks() {
+   axios.get('http://localhost:3000/tasks')
+  .then(response => this.tasks = response.data)
+  .catch(function (error) {
+  console.log(error);
+});
+},
 
+setResults () {
+        this.tasks = response.data;
+},
+created(){
+  this.getTasks()
+},
+}
 
-  
-  methods: {
-
-  },
-};
+}
 </script>
