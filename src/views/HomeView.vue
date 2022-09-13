cad3017e017b676b08a2ad76c74cb7d503003d3d
marilia-borgo <marilia.bm01@gmail.com>
2022-09-13 10:08:14 -0300

    get funcionou!

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,53 +1,55 @@
 <template>
-      <v-row justify="center" class="pa-md-16 mx-lg-auto">
+  <v-row justify="center" class="pa-md-16 mx-lg-auto">
+
     <v-expansion-panels inset>
-      <v-expansion-panel
-      v-for="tarefa in tasks" 
-      :key="tarefa.id">
-        <v-expansion-panel-header> {{ tarefa }} </v-expansion-panel-header>
+      <v-expansion-panel v-for="tarefa in tasks" :key="tarefa.id">
+        <v-expansion-panel-header> {{ tarefa.title }} </v-expansion-panel-header>
         <v-expansion-panel-content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
   </v-row>
 </template>
 
-
 <script>
-import axios from 'axios'
+
+import axios from "axios";
 export default {
- 
   name: "HomeView",
-  data: function(){
-  return{
-    tasks: [],
-    task: {
-      "id": '',
-      "title":'',
-      "dueTo": '',
-      "project": '',
-      "usuario": '',
-      isShow:false,
+  data: function () {
+    return {
+      tasks: [],
+      task: {
+        id: "",
+        title: "",
+        dueTo: "",
+        project: "",
+        usuario: "",
+        isShow: false,
+      },
+    };
+  },
+  methods: {
+    getTasks() {
+      axios
+        .get("http://localhost:3000/tasks/")
+        .then((response) => (this.tasks = response.data))
+        .catch(function (error) {
+          console.log(error);
+        });
     },
-  }
-},
-methods:{
-getTasks() {
-   axios.get('http://localhost:3000/tasks')
-  .then(response => this.tasks = response.data)
-  .catch(function (error) {
-  console.log(error);
-});
-},
 
-setResults () {
-        this.tasks = response.data;
-},
-created(){
-  this.getTasks()
-},
-}
+    setResults() {
+      this.tasks = response.data;
+    },
+  },
+  created() {
+      this.getTasks();
+    },
 
-}
+};
 </script>
