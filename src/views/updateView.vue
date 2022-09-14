91d46aa6872dd1f888ed336f3b5b38a3865465be
marilia-borgo <marilia.bm01@gmail.com>
2022-09-14 10:21:52 -0300

    quebrado

--- src/views/updateView.vue
+++ src/views/updateView.vue
@@ -1,4 +1,74 @@
 <template>
+
+<div>
+        <v-card
+    class="mx-auto px-16 py-16 mx-5 my-15"
+    max-width="344"
+    outlined>
+  <v-text-field
+            v-model="task.title"
+            label="Add Task"
+            outlined
+            clearable
+          ></v-text-field>
+
+          <v-select
+          v-model="task.project"
+          :items="items"
+          label="Outlined style"
+          outlined
+        ></v-select>
+        <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="task.dueTo"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="task.dueTo"
+            label="Picker in menu"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="task.dueTo"
+          no-title
+          scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.menu.save(task.dueTo)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+      <v-btn
+      v-on:click="post()"
+      class="ma-2"
+      outlined
+      color="indigo"
+    >
+      Adicionar tarefa
+    </v-btn>
+  </v-card>
+
     <v-main class="overflow-hidden mt-4">
       <v-container>
         <v-row>
@@ -12,12 +82,18 @@
         </v-row>
       </v-container>
     </v-main>
+</div>
   </template>
   
   <script>
   export default {
     data: () => {
       return {
+        items: [
+          'Front-end',
+          'Back-end',
+          'Dev-ops',
+        ],
         taskId: 0,
       }
     },
