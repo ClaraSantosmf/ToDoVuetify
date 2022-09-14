eacc945ca80bd09f5aabcd681665b8f68fe1dc35
ClaraSantosmf <clarasantosmf@gmail.com>
2022-09-14 11:23:39 -0300

    Inserido barra de filtrar

--- src/views/TasksListView.vue
+++ src/views/TasksListView.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <div>
+      <v-form>
+        <v-container>
+          <v-text-field v-model="pesquisa">
+            <template v-slot:label>
+              Pesquise uma task <strong>especifica</strong>
+              <v-icon style="vertical-align: middle"> mdi-file-find </v-icon>
+            </template>
+          </v-text-field>
+        </v-container>
+      </v-form>
+    </div>
     <v-btn
       class="mx-2 ml-15 mt-8"
       fab
@@ -7,60 +19,41 @@
       color="indigo"
       @click="$router.push('/adicionar')"
     >
-      <v-icon dark>
-        mdi-plus
-      </v-icon>
+      <v-icon dark> mdi-plus </v-icon>
     </v-btn>
-  <v-row justify="center" class="pa-md-16 mx-lg-auto">
+    <v-row justify="center" class="pa-md-16 mx-lg-auto">
+      <v-expansion-panels inset>
+        <v-expansion-panel v-for="tarefa in pesquisarTask" :key="tarefa.id">
+          <v-expansion-panel-header class="ml-5">
+            {{ tarefa.title }}
+          </v-expansion-panel-header>
 
-    <v-expansion-panels inset>
-      <v-expansion-panel v-for="tarefa in tasks" :key="tarefa.id">
-        <v-expansion-panel-header class='ml-5'> {{ tarefa.title }}
+          <v-expansion-panel-content>
+            <v-banner two-line>
+              <v-avatar slot="icon" color="deep-purple accent-4" size="40">
+                <v-icon icon="mdi-lock" color="white"> mdi-lock </v-icon>
+              </v-avatar>
 
-        </v-expansion-panel-header> 
-        
-        <v-expansion-panel-content>
-          <v-banner two-line>
-    <v-avatar
-      slot="icon"
-      color="deep-purple accent-4"
-      size="40"
-    >
-      <v-icon
-        icon="mdi-lock"
-        color="white"
-      >
-        mdi-lock
-      </v-icon>
-    </v-avatar>
-
-   
-
-    <template v-slot:actions>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-      >
-        Action
-      </v-btn>
-      <v-btn
-        text
-        color="deep-purple accent-4"
-        :to="{ name: 'taskUpdate', params: { id: tarefa.id } }"
-      >
-        Editar
-      </v-btn>
-    </template>
-  </v-banner>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </v-row>
-</div>
+              <template v-slot:actions>
+                <v-btn text color="deep-purple accent-4"> Action </v-btn>
+                <v-btn
+                  text
+                  color="deep-purple accent-4"
+                  :to="{ name: 'taskUpdate', params: { id: tarefa.id } }"
+                >
+                  Editar
+                </v-btn>
+              </template>
+            </v-banner>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+  </div>
 </template>
 
 <script>
-import T from "../TasksAPi.js"
+import T from "../TasksAPi.js";
 import axios from "axios";
 export default {
   name: "HomeView",
@@ -75,6 +68,7 @@ export default {
         usuario: "",
         isShow: false,
       },
+      pesquisa: "",
     };
   },
   methods: {
@@ -82,12 +76,18 @@ export default {
       this.tasks = response.data;
     },
   },
-  created() {
-    const populando = (response) => {
-      this.tasks = response
-    }
+  computed: {
+    pesquisarTask() {
+      return this.tasks.filter((e) =>
+        e?.title?.toLowerCase().includes(this.pesquisa.toLowerCase())
+      );
+    },
+    created() {
+      const populando = (response) => {
+        this.tasks = response;
+      };
       T.getTasks(populando);
     },
-
+  },
 };
 </script>
