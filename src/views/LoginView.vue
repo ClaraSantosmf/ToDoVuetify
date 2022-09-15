38d16761e16982c1f2391c67560587c4e6fbba59
ClaraSantosmf <clarasantosmf@gmail.com>
2022-09-15 09:15:33 -0300

    Funcionando o login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,83 @@
 <template>
-  <CadastroComponente></CadastroComponente>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar color="deep-purple" lighten-2>
+                <v-toolbar-title>Login</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    prepend-icon="mdi-account"
+                    name="login"
+                    label="User"
+                    type="text"
+                    v-model="username"
+                  ></v-text-field>
+                  <v-text-field
+                    id="password"
+                    prepend-icon="mdi-lock"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn color="deep-purple" lighten-2 @click="cadastrar()"
+                  >cadastre-se</v-btn
+                >
+                <v-btn color="deep-purple" lighten-2 @click="login()"
+                  >Logar</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import CadastroComponente from "@/components/cadastroComponente.vue";
+import axios from "axios";
+import TaskApi from "@/api/auth.api.js";
+
 export default {
-  name: "LoginUser",
-  props: {
-    source: String,
+  data: function () {
+    return {
+      username: "",
+      password: "",
+      loading: false,
+      valid: false,
+    };
   },
-  components: { CadastroComponente },
+  methods: {
+    async cadastrar() {
+      const user = this.usuario;
+      await axios.post("http://localhost:3000/user", user);
+      this.$router.push({ name: "home" });
+    },
+    login() {
+      this.loading = true;
+      TaskApi.login(this.username, this.password)
+        .then((resp) => {
+          console.log("login ok", resp);
+        })
+        .catch((error) => {
+          console.log("login falhou", error);
+          //mostrar toast de usuário ou senha inválidos
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+  },
 };
 </script>
-
-<style></style>
